84aa00c41f8b9cc3805bb2502cc0a54d2ea88695
Muhammad Ali <muhammad.ali.aktz@gmail.com>
2026-08-05 21:47:02 -0400

    Calculating Similarity

--- src/similarity.py
+++ src/similarity.py
@@ -1,3 +1,18 @@
 import fitz
-text = open("data/job_description.txt", "r").read()
-print(text)
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+from extract_reume import extract_resume
+
+
+
+with open("data/job_description.txt", "r", encoding="utf-8") as f:
+    job_text = f.read()
+document = fitz.open("data/resume.pdf")
+resume = extract_resume(document)
+vectorizer = TfidfVectorizer()
+tfidf_matrix = vectorizer.fit_transform([resume,job_text])
+
+
+similarity = cosine_similarity(tfidf_matrix[0:1], tfidf_matrix[1:2])
+print(f"Similarity: {similarity[0][0]*100:.2f}%")
