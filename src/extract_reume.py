5e42713817c054cc63c34f5c3da114353e19badc
Muhammad Ali <muhammad.ali.aktz@gmail.com>
2026-08-07 16:21:22 -0400

    doing comments

--- src/extract_reume.py
+++ src/extract_reume.py
@@ -1,7 +1,7 @@
-import fitz
-document = fitz.open("data/resume.pdf")
-def extract_resume(doc):
-    text = ""    
-    for page in doc:
-        text += page.get_text()
-    return text
+import fitz# for pdf processing
+document = fitz.open("data/resume.pdf") # open the resume pdf
+def extract_resume(doc): # extract the text from the resume pdf
+    text = "" # initialize the text
+    for page in doc: # iterate through the pages
+        text += page.get_text() # get the text from the page
+    return text # return the text
--- src/preprocess.py
+++ src/preprocess.py
@@ -1,7 +1,7 @@
-import fitz
-from extract_reume import extract_resume
-document = fitz.open("data/resume.pdf")
-text = extract_resume(document)
+import fitz # for pdf processing
+from extract_reume import extract_resume # for extracting text from pdf
+document = fitz.open("data/resume.pdf") # open the resume pdf
+text = extract_resume(document) # extract the text from the resume pdf
 def preprocess(text):
     text = text.lower()
     text = text.replace("\n", " ")  # remove new lines
