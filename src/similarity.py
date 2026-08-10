01088f3fb3968278f4c6d366b04a744cd96f699a
Muhammad Ali <muhammad.ali.aktz@gmail.com>
2026-08-09 20:24:31 -0400

    update

--- src/similarity.py
+++ src/similarity.py
@@ -5,20 +5,23 @@ from sklearn.metrics.pairwise import cosine_similarity
 from extract_reume import extract_resume
 
 # Step 1: Load the job description and resume
+def similarity(job_description, resume):
 
 # Load the job description
-with open("data/job_description.txt", "r", encoding="utf-8") as f:
-    job_text = f.read()
+    with open("data/job_description.txt", "r", encoding="utf-8") as f:
+        job_text = f.read()
 
-# Load the resume
-document = fitz.open("data/resume.pdf")
-resume = extract_resume(document)
+    # Load the resume
+    document = fitz.open("data/resume.pdf")
+    resume = extract_resume(document)
 
-# Step 2: Vectorize the job description and resume
-vectorizer = TfidfVectorizer()
-tfidf_matrix = vectorizer.fit_transform([resume,job_text])
- 
+    # Step 2: Vectorize the job description and resume
+    vectorizer = TfidfVectorizer()
+    tfidf_matrix = vectorizer.fit_transform([resume,job_text])
+    
 
-# Step 3: Calculate the similarity between the job description and resume
-similarity = cosine_similarity(tfidf_matrix[0:1], tfidf_matrix[1:2])
-print(f"Similarity: {similarity[0][0]*100:.2f}%")
+    # Step 3: Calculate the similarity between the job description and resume
+    similarity = cosine_similarity(tfidf_matrix[0:1], tfidf_matrix[1:2])
+
+    score = similarity[0][0]*100
+    return score
